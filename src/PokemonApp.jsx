7655b3fd77422dd7fbbe504b2423125973d6872c
Santiago Cifuentes <cifuentess938@gmail.com>
2022-11-27 19:48:05 -0500

    first commit and finished the introducction to redux

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPokemons } from "./store/slices/pokemon/thunks";
+
+export const PokemonApp = () => {
+  const { pokemons =[], isLoading, page } = useSelector((state) => state.pokemons);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPokemons());
+  }, []);
+  console.log(pokemons);
+  console.log(isLoading);
+  return (
+    <>
+      <h1>PokemonApp</h1>
+      <hr />
+      <span>Loading: {isLoading ? "True" : "False"}</span>
+
+      <ul>
+        {
+          pokemons.map(({name}) => (
+          <li key={name}>{ name}</li>
+          ))
+        }
+      </ul>
+
+      <button disabled={isLoading} onClick={() => dispatch(getPokemons(page))}>
+        Next
+      </button>
+    </>
+  );
+};
